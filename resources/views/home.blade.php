2e0c700ad8f8e5a3aed88dc3feaa9f39ede36c76
PasqualeIannace <pasquale.iannace@gmail.com>
2024-01-19 17:55:00 +0100

    Creato template yield

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DC Comics</title>
-    <link rel="icon" type="image/x-icon" href="/favicon.ico">
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-
-<body>
-    <header>
-        @include('partials.nav')
-    </header>
-</body>
-
-</html>
+@extends('layouts.app')
