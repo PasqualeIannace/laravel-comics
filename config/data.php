3d39e998e7ab7dcb252de39259dacc233f7550b1
PasqualeIannace <pasquale.iannace@gmail.com>
2024-01-20 10:36:23 +0100

    Creata section 2

--- config/data.php
+++ config/data.php
@@ -125,4 +125,32 @@ return [
             "type" => "graphic novel",
         ]
     ],
+
+    "other" => [
+        [
+            "text" => "DIGITAL COMICS",
+            "url" => "#",
+            "src" => "buy-comics-digital-comics.png"
+        ],
+        [
+            "text" => "DC MERCHANDISE",
+            "url" => "#",
+            "src" => "buy-comics-merchandise.png"
+        ],
+        [
+            "text" => "SUBSCRIPTION",
+            "url" => "#",
+            "src" => "buy-comics-subscriptions.png"
+        ],
+        [
+            "text" => "COMIC SHOP LOCATOR",
+            "url" => "#",
+            "src" => "buy-comics-shop-locator.png"
+        ],
+        [
+            "text" => "DC POWER VISA",
+            "url" => "#",
+            "src" => "buy-dc-power-visa.svg"
+        ]
+    ]
 ];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
-
+{{-- JUMBO --}}
 @section('content')
     <div class="jumbo">
         <img src="/jumbotron.jpg" alt="">
     </div>
-
+{{-- SECTION 1 --}}
     <div class="productsBg">
 		<div class="desktop products">
 			<div class="info white">
@@ -26,4 +26,23 @@
 			</div>
 		</div>
 	</div>
+
+	{{-- SECTION 2 --}}
+	<div class="sectionBg">
+        <div class="desktop section">
+			@foreach ($other as $item)
+            <div class="product">
+				<a href={{$item["url"]}}>
+                    <div>
+                        <img class="other" src={{$item["src"]}} alt="">
+                    </div>
+                    <div class="px1">
+                        {{ $item["text"] }}
+                    </div>
+            </a> 
+		</div>
+				@endforeach
+            
+        </div>
+    </div>
 @endsection
